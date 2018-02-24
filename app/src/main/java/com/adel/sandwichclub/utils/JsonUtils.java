647b125d8d3055152468b28ee545a82b88701a58
Adel <adel.othman@live.com>
2018-02-24 11:34:03 +0100

    first

---
+++ app/src/main/java/com/adel/sandwichclub/utils/JsonUtils.java
@@ -0,0 +1,10 @@
+package com.adel.sandwichclub.utils;
+
+import com.adel.sandwichclub.model.Sandwich;
+
+public class JsonUtils {
+
+    public static Sandwich parseSandwichJson(String json) {
+        return null;
+    }
+}
